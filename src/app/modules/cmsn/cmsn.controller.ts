bd33a76f8c8fa61efc9a89537d7a6537a6e611a1
Lê Thành Hiếu <hieule.9420@gmail.com>
2021-11-26 22:02:44 +0700

    feat: birthday event

--- src/app/modules/cmsn/cmsn.controller.ts
+++ src/app/modules/cmsn/cmsn.controller.ts
@@ -4,10 +4,18 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class CmsnController {
     constructor()
     {}
-
     @Get()
     @Render('pages/cmsn/cmsn')
     async root(){
-        return;
+        let toDate = new Date().getTime();
+        let dateEvent = new Date(2021, 11, 6).getTime();
+
+        let birthDay = false;
+        if((dateEvent - toDate) <= 0){
+            birthDay = true;
+        }
+        return{
+            birthDay
+        };
     }
 }
